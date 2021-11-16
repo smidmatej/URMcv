80f85e69d1253389666a8de3d6ad4303499d9c91
vilda-zan <vilda.zan@gmail.com>
2021-11-16 12:31:35 +0100

    dvojite kyvadlo na voziku

---
+++ Janička cv - Vili/november16.m
@@ -0,0 +1,11 @@
+%% Simulink xxx Simscape(Simmechanics)
+
+% Simulink je kauzalni modelovani
+% Simscape je akauzalni - mezi blokama nejsou sipky
+% Simscape udela dif. soustavu a resi ji najednou
+
+%% jedhoduche kyvadlo
+
+l = 5; % delka zavesu
+m = 1; % hmotny bod
+phi0 = 90;
