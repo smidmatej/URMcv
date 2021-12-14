bad333ef8b79c5f6059448fe6bde0d14c241bb14
smidmatej <smidmatej99@gmail.com>
2021-12-14 22:30:46 +0100

    DMG_IGM a manipulator funguje

--- setup.m
+++ setup.m
@@ -2,9 +2,19 @@ a1 = 2
 a2 = 1.5
 a3 = 0.5
 
+%pracovni prostor
+r = a1-a2-a3 % nejvetsi - suma mensich je asi vnitrni polomer
+R = a1+a2+a3 % kam az dosahne
+
+% parametry sme urcite dostali aby r = 0
+% 
+% a1 = 3
+% a2 = 1
+% a3 = 0.5
+
 theta10 = 0
-theta20 = pi/2
-theta30 = pi
+theta20 = 0
+theta30 = 0
 T_1_0 = [cos(theta10), -sin(theta10), 0, a1*cos(theta10);
     sin(theta10), cos(theta10), 0, a1*sin(theta10);
     0 0 1 0;
