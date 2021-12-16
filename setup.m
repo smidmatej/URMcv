923a60f322fa10528e8945929b1d8a147eded178
vilda-zan <vilda.zan@gmail.com>
2021-12-16 13:49:16 +0100

    poku ioku

--- setup.m
+++ setup.m
@@ -28,3 +28,12 @@ T_3_2 = [cos(theta30), -sin(theta30), 0, a3*cos(theta30);
     0 0 1 0;
     0 0 0 1]
 % [x,y,phi] = DGM(1,1,1,a1,a2,a3)
+
+%% zlo v podobe symbolickeho toolboxu
+syms a3 a2 a1 theta1 theta2 theta3 
+
+JQ = [-a3 * sin(theta1 + theta2 + theta3) - a2 * sin(theta1 + theta2) - a1 * sin(theta1), -a3 * sin(theta1 + theta2 + theta3) - a2 * sin(theta1 + theta2), -a3 * sin(theta1 + theta2 + theta3);...
+       a3 * cos(theta1 + theta2 + theta3) + a2 * cos(theta1 + theta2) + a1 * cos(theta1), a3 * cos(theta1 + theta2 + theta3) + a2 * cos(theta1 + theta2), a3 * cos(theta1 + theta2 + theta3);...
+       1, 1, 1];
+vysledek = inv(JQ)   
+   
