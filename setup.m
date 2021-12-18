ae6d67be2e4f074a00336d143f128f2288e7fbec
smidmatej <smidmatej99@gmail.com>
2021-12-18 10:36:31 +0100

    Singularni polohy

--- setup.m
+++ setup.m
@@ -31,10 +31,10 @@ T_3_2 = [cos(theta30), -sin(theta30), 0, a3*cos(theta30);
 % [x,y,phi] = DGM(1,1,1,a1,a2,a3)
 
 %% zlo v podobe symbolickeho toolboxu
-syms a3 a2 a1 theta1 theta2 theta3 
-
-JQ = [-a3 * sin(theta1 + theta2 + theta3) - a2 * sin(theta1 + theta2) - a1 * sin(theta1), -a3 * sin(theta1 + theta2 + theta3) - a2 * sin(theta1 + theta2), -a3 * sin(theta1 + theta2 + theta3);...
-       a3 * cos(theta1 + theta2 + theta3) + a2 * cos(theta1 + theta2) + a1 * cos(theta1), a3 * cos(theta1 + theta2 + theta3) + a2 * cos(theta1 + theta2), a3 * cos(theta1 + theta2 + theta3);...
-       1, 1, 1];
-vysledek = inv(JQ)   
+% syms a3 a2 a1 theta1 theta2 theta3 
+% 
+% JQ = [-a3 * sin(theta1 + theta2 + theta3) - a2 * sin(theta1 + theta2) - a1 * sin(theta1), -a3 * sin(theta1 + theta2 + theta3) - a2 * sin(theta1 + theta2), -a3 * sin(theta1 + theta2 + theta3);...
+%        a3 * cos(theta1 + theta2 + theta3) + a2 * cos(theta1 + theta2) + a1 * cos(theta1), a3 * cos(theta1 + theta2 + theta3) + a2 * cos(theta1 + theta2), a3 * cos(theta1 + theta2 + theta3);...
+%        1, 1, 1];
+% vysledek = inv(JQ)   
    
