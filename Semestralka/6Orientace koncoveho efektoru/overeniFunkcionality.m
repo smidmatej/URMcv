f1e5a4ca8d8a1447a3c912b5c9371693fe791884
vilda-zan <vilda.zan@gmail.com>
2021-12-21 12:57:50 +0100

    zmeny od Viliho v šestce

--- Semestralka/6Orientace koncoveho efektoru/overeniFunkcionality.m
+++ Semestralka/6Orientace koncoveho efektoru/overeniFunkcionality.m
@@ -1,39 +1,36 @@
-close all
 clear all
-clc
+a1 = 2
+a2 = 1.5
+a3 = 0.5
 
-%% Parametry a simulace
-a1 = 2;
-a2 = 1.5;
-a3 = 0.5;
+r = a1-a2-a3 % nejvetsi - suma mensich je asi vnitrni polomer
+R = a1+a2+a3 % kam az dosahne
 
-r = a1-a2-a3; % nejvetsi - suma mensich je vnitrni polomer
-R = a1+a2+a3; % pracovni prostor
+v = 0.5
+v2 = 2
+smax = 3.9769
+Tmax = smax/v
 
-v = 0.5;
-v2 = 2;
-smax = 3.9769;
-Tmax = smax/v;
-
-theta10 = 0;
-theta20 = 0;
-theta30 = 0;
+theta10 = 0
+theta20 = 0
+theta30 = 0
 T_1_0 = [cos(theta10), -sin(theta10), 0, a1*cos(theta10);
     sin(theta10), cos(theta10), 0, a1*sin(theta10);
     0 0 1 0;
-    0 0 0 1];
+    0 0 0 1]
 T_2_1 = [cos(theta20), -sin(theta20), 0, a2*cos(theta20);
     sin(theta20), cos(theta20), 0, a2*sin(theta20);
     0 0 1 0;
-    0 0 0 1];
+    0 0 0 1]
 T_3_2 = [cos(theta30), -sin(theta30), 0, a3*cos(theta30);
     sin(theta30), cos(theta30), 0, a3*sin(theta30);
     0 0 1 0;
-    0 0 0 1];
+    0 0 0 1]
+% [x,y,phi] = DGM(1,1,1,a1,a2,a3)
 
-simulation = sim('trajektorie_sim');
 
-%% Vykresleni
+simulation = sim('OvereniPOKUIOKU');
+%%
 figure
 subplot(3,1,1)
 hold on
